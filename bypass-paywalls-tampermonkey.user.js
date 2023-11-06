c8922bce175ca9e8a72421b591fdd732af472f09
novohool <jingslunt@qq.com>
2023-11-06 14:05:04 +0800

    Update bypass-paywalls-tampermonkey.user.js

--- bypass-paywalls-tampermonkey.user.js
+++ bypass-paywalls-tampermonkey.user.js
@@ -393,7 +393,7 @@
         removeDOMElement(paywall);
     } else if (matchDomain('bloomberg.com')) {
         blockElement('#graphics-paywall-overlay', true);
-        blockElement('[role="alertdialog"]', true);
+        blockElement('#fortress-paywall-container-root', true);
     } else if (matchDomain('bloombergquint.com')) {
         const articlesLeftModal = document.getElementsByClassName('paywall-meter-module__story-paywall-container__1UgCE')[0];
         const paywall = document.getElementById('paywallDmp');
